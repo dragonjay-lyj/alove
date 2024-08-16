ecdf42521601b3cd9efea3bdb1d74461d7143d21
dragonjay-lyj <liujinlisb@gmail.com>
2024-08-16 18:42:57 +0800

    new file:   src/components/fundoshitableen.tsx 	new file:   src/content/docs/en/feedback/answer.astro

--- src/components/fundoshitableen.tsx
+++ src/components/fundoshitableen.tsx
@@ -12,53 +12,53 @@ export default function App() {
       <TableBody>
         <TableRow key="1">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417581/Fundoshi/6_01_fhrk3h.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【步骤一】
-将褌的一端（イ）搭在肩上，用另一端（ロ）遮住裆部，从两腿之间绕到身后。当然，实际上是用手按住的。</TableCell>
+          <TableCell>【Step 1】
+Drape one end (イ) of the fundoshi over your shoulder. With the other end (ロ), cover your groin area and pass it through your legs towards your back. Naturally, you will hold it in place with your hand.</TableCell>
         </TableRow>
         <TableRow key="2">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417580/Fundoshi/6_02_ur1s7c.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【步骤二】
-将从裆部绕过来的那一端（ロ）斜着从臀部绕到腰间，从搭在肩上的那一端（イ）上方交叉，从上往下看，绕腰部逆时针缠绕一周。（イ）仍然搭在肩上。</TableCell>
+          <TableCell>【Step 2】
+Bring end (ロ), which has passed between your legs, around your waist while diagonally crossing over your buttocks. Cross it over end (イ), which is still draped over your shoulder, and wrap it around your waist counterclockwise. </TableCell>
         </TableRow>
         <TableRow key="3">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417580/Fundoshi/6_03_yftt6s.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【步骤三】
-将（ロ）从内侧勾住横跨臀部的布料，向反方向拉紧。</TableCell>
+          <TableCell>【Step 3】
+Hook end (ロ) from the inside onto the fabric crossing over your buttocks and pull it in the opposite direction.</TableCell>
         </TableRow>
         <TableRow key="4">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417581/Fundoshi/6_04_sx3p0s.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【步骤四】
-将（ロ）端塞进腰带（横跨臀部的布料），暂时固定。</TableCell>
+          <TableCell>【Step 4】
+(ロ) is temporarily tucked under the horizontal wrap (the fabric circling the waist like a belt).</TableCell>
         </TableRow>
         <TableRow key="5">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6_05_zck3cs.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【步骤五】
-将搭在肩上的（イ）端放下，使其自然垂在身前。</TableCell>
+          <TableCell>【Step 5】
+Bring (イ) down from the shoulder and let it hang in front.</TableCell>
         </TableRow>
         <TableRow key="6">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6_06_rdqw88.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【步骤六】
-将搭在肩上的布端（イ）从胯下穿过绕到身后，然后解开（ロ）的固定。</TableCell>
+          <TableCell>【Step 6】
+Pass (イ) between the legs and towards the back. Then, release the temporary hold on (ロ).</TableCell>
         </TableRow>
         <TableRow key="7">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6a_07_jecusr.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【步骤七】
-从背后看，将（イ）、（ロ）两端交叉，（イ）在左，（ロ）在右，左右用力拉紧。</TableCell>
+          <TableCell>【Step 7】
+Cross (イ) and (ロ) at the back, with (イ) on the left and (ロ) on the right from a rear view. Pull both sides firmly to tighten.</TableCell>
         </TableRow>
         <TableRow key="8">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6a_08_rcwuva.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【步骤八】
-将（イ）和（ロ）从下往上绕过腰带。</TableCell>
+          <TableCell>【Step 8】
+Tuck (イ) and (ロ) upwards into the horizontal wrap from below.</TableCell>
         </TableRow>
         <TableRow key="9">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417581/Fundoshi/6a_09_z5jdtq.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【完成图一】
-背后观赏图。</TableCell>
+          <TableCell>【Completed 1】
+Rear view.</TableCell>
         </TableRow>
         <TableRow key="10">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6_10_tzzyrr.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>【完成图二】
-正面观赏图。请务必将前襟（前方三角形的部分）整理平整。</TableCell>
+          <TableCell>【Completed 2】
+Front view. Make sure to spread and adjust the front pouch (the triangular pouch in front).</TableCell>
         </TableRow>
       </TableBody>
     </Table>
