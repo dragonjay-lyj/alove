67e9fd63c373301a9af456ceabf4990c7785655d
dragonjay-lyj <liujinlisb@gmail.com>
2024-09-26 17:55:23 +0800

    modified:   src/components/fundoshitable.tsx 	modified:   src/components/tabitable.tsx

--- src/components/fundoshitable.tsx
+++ src/components/fundoshitable.tsx
@@ -13,52 +13,52 @@ export default function App() {
         <TableRow key="1">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417581/Fundoshi/6_01_fhrk3h.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【步骤一】
-将褌的一端（イ）搭在肩上，用另一端（ロ）遮住裆部，并从裆部绕至身后。当然，实际操作中需要用手按住。</TableCell>
+将褌一端（イ）搭于肩上，另一端（ロ）遮蔽私处，并绕至身后。自然，此步骤需以手扶持。</TableCell>
         </TableRow>
         <TableRow key="2">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417580/Fundoshi/6_02_ur1s7c.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【步骤二】
-将从裆部绕至身后的（ロ）端，斜着穿过臀部，绕至腰部，并从（イ）端上方穿过，使褌在腰部绕一圈（从上方看为逆时针方向）。（イ）端依然搭在肩上。</TableCell>
+将绕至身后之（ロ）端，斜跨臀部，缠绕腰际，并穿过（イ）端上方，依反时针方向绕腰一周。（イ）端仍搭于肩上。</TableCell>
         </TableRow>
         <TableRow key="3">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417580/Fundoshi/6_03_yftt6s.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【步骤三】
-将（ロ）端从内侧勾住穿过臀部的布料，向反方向拉紧。</TableCell>
+将（ロ）端，由内向外，勾住斜跨臀部之布料，并向反方向拉紧。</TableCell>
         </TableRow>
         <TableRow key="4">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417581/Fundoshi/6_04_sx3p0s.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【步骤四】
-将（ロ）端夹在横绕腰部的布料（如同腰带般）之间，暂时固定。</TableCell>
+将（ロ）端塞入横绕腰际之布料（如同腰带一般）中，暂时固定。</TableCell>
         </TableRow>
         <TableRow key="5">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6_05_zck3cs.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【步骤五】
-将搭在肩上的（イ）端放下，使其自然垂在身前。</TableCell>
+将搭于肩上之（イ）端放下，使其自然垂于身前。</TableCell>
         </TableRow>
         <TableRow key="6">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6_06_rdqw88.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【步骤六】
-将（イ）端从裆部绕至身后，并解开（ロ）端的临时固定。</TableCell>
+将（イ）端穿过私处，绕至身后。并解开（ロ）端之临时固定。</TableCell>
         </TableRow>
         <TableRow key="7">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6a_07_jecusr.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【步骤七】
-从身后看，将（イ）端置于左侧，（ロ）端置于右侧，并将两者交叉，向左右两侧用力拉紧。</TableCell>
+将身后之（イ）（ロ）两端交叉，（イ）位于左侧，（ロ）位于右侧，并向两侧拉紧固定。</TableCell>
         </TableRow>
         <TableRow key="8">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6a_08_rcwuva.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【步骤八】
-将（イ）端和（ロ）端从下往上塞进横绕腰部的布料中。</TableCell>
+将（イ）（ロ）两端，由下至上，卷入横绕腰际之布料中。</TableCell>
         </TableRow>
         <TableRow key="9">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417581/Fundoshi/6a_09_z5jdtq.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【完成图一】
-后视图。</TableCell>
+后视图</TableCell>
         </TableRow>
         <TableRow key="10">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722417579/Fundoshi/6_10_tzzyrr.gif" loading="lazy" isZoomed alt="Midas" /></TableCell>
           <TableCell>【完成图二】
-前视图。注意将前袋（前方三角形的兜裆布）整理平整。</TableCell>
+前视图。将前褌（前方三角形部分）整理平整</TableCell>
         </TableRow>
       </TableBody>
     </Table>
--- src/components/tabitable.tsx
+++ src/components/tabitable.tsx
@@ -12,16 +12,16 @@ export default function App() {
       <TableBody>
         <TableRow key="1">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722915777/Fundoshi/w_tabi_1_jsn6v7.avif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>正确的穿着顺序是，先穿好褌或灯笼裤等下半身内衣，再穿足袋。若要穿着肌襦袢或衬裙，也应在穿好足袋之后，以免衣着散乱。至少在穿长襦袢之前，要先穿好足袋。
-          穿足袋时，应如左图所示，单腿站立，身体略微前倾。盘腿而坐穿足袋，并非正确的姿势。</TableCell>
+          <TableCell>着褌或衬裤等亵衣后，方可穿足袋。若着肌襦袢或衬裙，亦应先穿足袋，以免衣衫走样。至少在穿长襦袢之前，应先穿好足袋。
+          穿足袋之时，宜单腿站立，屈膝弯腰，如左图所示。盘腿而坐穿足袋，实为不雅之姿。</TableCell>
         </TableRow>
         <TableRow key="2">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722916073/Fundoshi/w_tabi_2_n45prw.avif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>穿足袋时，应如左图所示，将足袋向外翻折一半，套在脚上，并用双手将足尖向后拉至底部。然后将翻折部分复原，包裹住脚跟。</TableCell>
+          <TableCell>穿足袋，先将足袋外翻至半，套入足中，以双手将趾尖拉至最深处，再将外翻部分复原，包裹至脚跟，如左图所示。</TableCell>
         </TableRow>
         <TableRow key="3">
           <TableCell><Image src="https://res.cloudinary.com/dkhp3hucr/image/upload/v1722915976/Fundoshi/w_tabi_3_butgki.avif" loading="lazy" isZoomed alt="Midas" /></TableCell>
-          <TableCell>系小钩时，务必按照从下往上的顺序，将小钩依次挂在挂绳上。每处挂绳都有两条，靠近内侧的是备用绳。将小钩挂在外侧的挂绳上才是正确的固定位置，此时测量的尺寸才是准确的足袋尺寸。选择足袋尺寸时，务必将小钩挂在外侧挂绳上进行测量。如果小钩难以固定，可以抓住脚踝向外或向内扭动，以便固定。另外，脱足袋时则无需讲究顺序，随意即可。</TableCell>
+          <TableCell>系小钩，须自下而上，依次扣于挂线。挂线有两根，靠内侧者为备用。将小钩扣于外侧挂线，方为正确位置，此时尺寸最为精准。选购足袋，亦应以外侧挂线为准。若小钩不易扣合，可将脚踝向外或向内扭转。脱足袋则无需讲究。</TableCell>
         </TableRow>
       </TableBody>
     </Table>
